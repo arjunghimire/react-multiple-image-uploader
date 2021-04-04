edf2c0d61e1a26355701e217991ddec6f151a6e7
jaredLo <gerardlo.psd@gmail.com>
2021-04-04 13:13:38 +0800

    Revert "Added isOpen and hideModal to RIUploaderProps, ? to warnMessage"
    This reverts commit 9777088e0fbb34bc62aebf6795e9318829556de1.

--- dist/types.d.ts
+++ dist/types.d.ts
@@ -3,12 +3,10 @@ interface DataSource {
     dataURL: string;
 }
 interface RIUploaderProps {
-    isOpen: boolean;
-    hideModal: () => void;
     onUpload: (e: any) => void;
     onSelect: (e: any) => void;
     onRemove: (e: any) => void;
-    warnMessage?: string;
+    warnMessage: string;
     dataSources: DataSource[];
 }
 export default RIUploaderProps;
